d3133d1e2272f45aad860e0007ba69bb846c3a6c
Mohammad Sadeghi <mohammad2184@gmail.com>
2025-11-16 15:05:05 -0800

    created separate files for core logic/processing of audio and gui. Included requirements text file for installing needed dependency.

---
+++ gui.py
@@ -0,0 +1,8 @@
+'''
+Contains the GUI (tKinter) code.
+
+Responsibilities:
+  - Layout and styling of the application window
+  - Handling user interaction (buttons, file dialogs)
+  - Calling functions in processing.py to do the actual audio work
+'''
---
+++ processing.py
@@ -0,0 +1,9 @@
+''' 
+Contains all processing code. 
+
+Responsibilities:
+  - Reading input and noise audio files
+  - Performing noise cancellation / filtering
+  - Returning processed audio to the GUI
+  - Saving processed audio to disk
+'''
